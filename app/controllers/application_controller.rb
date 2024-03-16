9669dabd434a4950451ad6b5bf9da0e7791a2c2b
leorchrd <leoonardorichard@gmail.com>
2024-03-15 21:20:41 -0300

    creating the layout for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  layout :layout_by_resource
+
+  def layout_by_resource
+    if devise_controller? && resource_class == Admin
+      'admins_devise'
+    else
+      'application'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :admins_backoffice do
     get 'welcome/index'
-    resources :admins, only: [:index, :edit]
+    resources :admins
   end
   namespace :site do
     get 'welcome/index'
@@ -13,8 +13,17 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :admins
 
+  # Rotas de logout para usuários e administradores
+  devise_scope :user do
+    get '/users/sign_out', to: 'devise/sessions#destroy', as: :user_logout
+  end
+
+  devise_scope :admin do
+    get '/admins/sign_out', to: 'devise/sessions#destroy', as: :admin_logout
+  end
+
   get 'inicio', to: 'site/welcome#index'
 
   root to: 'site/welcome#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Para mais detalhes sobre a DSL disponível neste arquivo, consulte http://guides.rubyonrails.org/routing.html
 end
