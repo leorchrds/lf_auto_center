f122c17cf3db08b3063fd0162cdfd32cc59198fb
leorchrds <leoonardorichard@gmail.com>
2024-03-30 16:26:04 -0300

    Updated the relationships in the Client, Address, and Vehicle models to correctly reflect the association with ServiceOrder.

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,6 @@
 class Client < ApplicationRecord
   validates :name, :phone, presence: true
-  has_one :address
+  has_one :address, dependent: :destroy  # Relacionamento com endereço, o endereço será destruído se o cliente for
   has_many :service_orders
-  accepts_nested_attributes_for :address
+  accepts_nested_attributes_for :address  # Permitir atributos aninhados para o endereço
 end
